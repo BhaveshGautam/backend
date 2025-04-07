f8c34d1b8c874c454929c9723ed1be654183df22
Bhavesh Gautam <bhaveshgautam2003@gmail.com>
2025-04-07 23:43:29 +0530

    backend

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { createCourse, getAllCoursesWithUsers,getSingleCourse, getCourseAttendance} 
+from "../controllers/courseController.js";
 
-import { createCourse, getAllCoursesWithUsers, getSingleCourse ,getCourseAttendance} from "../controllers/courseController.js";
 const router = express.Router();
 
 router.get("/all", getAllCoursesWithUsers);
-router.get("/:id", getSingleCourse); 
 router.post("/", createCourse);
 router.get("/:id/attendance", getCourseAttendance);
+router.get("/:id", getSingleCourse);
+
+export default router;
+
 // router.put("/:id", updateCourse);
 // router.delete("/:id", deleteCourse);
